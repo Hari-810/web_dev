ae7c91924bd6f00e69d9e98c92587de169e29ad2
Hari-810 <harijee098@gmail.com>
2023-10-26 10:10:19 +0530

    updated

--- JavaScript/11/DOM_styling_js/script.js
+++ JavaScript/11/DOM_styling_js/script.js
@@ -3,7 +3,7 @@ divElement.style.backgroundColor = "red";
 divElement.style.color = "white";
 divElement.style.fontSize = "20px";
 
-
+console.log(document)
 var h1element = document.getElementById("hI");
 h1element.style.backgroundColor = "blue";
 
