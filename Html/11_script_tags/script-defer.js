8520b999cae08047de06a49d8df2dc1213e88c04
hv <harijee098@gmail.com>
2024-04-09 14:44:09 +0530

    html updated

---
+++ Html/11_script_tags/script-defer.js
@@ -0,0 +1,4 @@
+// script-defer.js - External JavaScript file with 'defer'
+document.addEventListener("DOMContentLoaded", function () {
+    console.log("script-defer.js is executed after HTML content is loaded.");
+});
