7d08b55b4bf0968c558f35a05e9a17c0abc47d54
hv <harijee098@gmail.com>
2024-08-20 08:57:10 +0530

    updated

---
+++ JavaScript/04/DOM_Modification/calc/DOM_styling_js/script.js
@@ -0,0 +1,10 @@
+var divElement = document.getElementById("myDiv");
+divElement.style.backgroundColor = "red";
+divElement.style.color = "white";
+divElement.style.fontSize = "20px";
+
+console.log(document)
+var h1element = document.getElementById("hI");
+h1element.style.backgroundColor = "blue";
+
+.getElement
