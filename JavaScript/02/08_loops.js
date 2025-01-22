465ff93b5922ef24ba88acdabd9a9cc394163e7d
hv <harijee098@gmail.com>
2025-01-22 11:07:24 +0530

    loops updated

--- JavaScript/02/08_loops.js
+++ JavaScript/02/08_loops.js
@@ -3,7 +3,6 @@ What is loop ?
     A loop in JavaScript is a control flow statement that allows you to repeatedly execute a block of code. 
 */
 
-
 // For loop
 /*
 The for loop in JavaScript is a control flow statement that allows you to repeatedly execute a block of code a specific number of times. The basic syntax of a for loop is as follows
@@ -15,13 +14,17 @@ for (initialization; condition; increment/decrement) {
 }
 
 */
-for (var i = 0; i < 10; i++) {
-    console.log(i);
-    }
-   
+for (let i = 2; i <= 10; i += 2) {
+  console.log(i);
+}
 
-    
-// for...of loop     
+for (let i = 0; i <= 10; i++) {
+  if (i % 2 === 0) {
+    console.log(i);
+  }
+}
+
+// for...of loop
 /*
 The for...of loop in JavaScript is a control flow statement that allows you to iterate over the items of an array, string, or other iterable objects. The basic syntax of a for...of loop is as follows
 
@@ -32,9 +35,23 @@ for (variable of iterable) {
 }
 */
 
+// let array = [1, 2, 3, 4, 5];
+// for (const item of array) {
+// console.log(item);
+// }
+
+// let array = [1, 2, 3, 4, 5];
+// for (const item of array) {
+//   if (item % 2 === 0) {
+//     console.log(item);
+//   }
+// }
+
 let array = [1, 2, 3, 4, 5];
-for (const item of array) {
-console.log(item);
+for (let i = 0; i < array.length; i++) {
+  if (array[i] % 2 === 0) {
+    console.log(array[i]);
+  }
 }
 
 // for...in loop
@@ -48,13 +65,10 @@ for (variable in object) {
 }
 */
 
-
-let obj = {a: 1, b: 2, c: 3};
+let obj = { a: 1, b: 2, c: 3 };
 for (const key in obj) {
-console.log(`key: ${key} , value: ${obj[key]}`);
+  console.log(`key: ${key} , value: ${obj[key]}`);
 }
-
-
 
 // while loop
 
@@ -70,12 +84,11 @@ while (condition) {
 
 var i = 0;
 while (i < 10) {
-console.log(i);
-i++;
+  console.log(i);
+  i++;
 }
 
-
-// do...while loop 
+// do...while loop
 /*
 A do...while loop in JavaScript is a control flow statement that is similar to a while loop, but it guarantees that the code inside the loop will be executed at least once. The basic syntax of a do...while loop is as follows
 
@@ -84,9 +97,8 @@ do {
 } while (condition);
 */
 
-
 let i = 0;
 do {
-console.log(i);
-i++;
+  console.log(i);
+  i++;
 } while (i < 10);
